07ce216fd5e57cbabf8c8c2f7736ef38e1f4f3d2
andihasan97 <andihasanelfalakiy@gmail.com>
2024-08-31 22:04:45 +0700

    delete: unused library

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -27,11 +27,7 @@ dependencies {
 
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
-    // This dependency is exported to consumers, that is to say found on their compile classpath.
-    api(libs.commons.math3)
-
-    // This dependency is used internally, and not exposed to consumers on their own compile classpath.
-    implementation(libs.guava)
+    
 }
 
 // Apply a specific Java toolchain to ease working on different environments.
