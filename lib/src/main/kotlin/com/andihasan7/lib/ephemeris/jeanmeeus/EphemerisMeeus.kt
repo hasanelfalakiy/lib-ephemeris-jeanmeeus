fd839341c0af35b82c7d5ba1c913893b1ed2d52b
andihasan97 <andihasanelfalakiy@gmail.com>
2024-09-02 23:20:36 +0700

    hitung rumus sampai vector radius au

--- lib/src/main/kotlin/com/andihasan7/lib/ephemeris/jeanmeeus/EphemerisMeeus.kt
+++ lib/src/main/kotlin/com/andihasan7/lib/ephemeris/jeanmeeus/EphemerisMeeus.kt
@@ -2,6 +2,10 @@
 
 package com.andihasan7.lib.ephemeris.jeanmeeus
 
+import kotlin.math.cos
+import kotlin.math.sin
+import kotlin.math.tan
+import kotlin.math.PI
 import com.andihasan7.lib.ephemeris.jeanmeeus.util.masehiToJD
 import com.andihasan7.lib.ephemeris.jeanmeeus.util.toDegreeFullRound2
 
@@ -90,6 +94,85 @@ class EphemerisMeeus(
     val trueObliquityOfEcliptic = Nutasi.deltaPsiDanEpsilon(nilaiT)[6]
     val trueObliquityOfEclipticDMS = toDegreeFullRound2(trueObliquityOfEcliptic)
     
+	/**
+	 * earth heliocentric longitude radians
+	 */
+	val earthHeliocentricLongitudeRadians = TabelMatahari.bujurEkliptik(tau, nilaiT)[0]
+	
+	/**
+	 * earth heliocentric longitude degrees 
+	 */
+	val earthHeliocentricLongitudeDegrees = TabelMatahari.bujurEkliptik(tau, nilaiT)[1]
+    val earthHeliocentricLongitudeDegreesDMS = toDegreeFullRound2(earthHeliocentricLongitudeDegrees)
+	
+	/**
+	 * sun geometric longitude degrees
+	 */
+	val sunGeometricLongitudeDegrees = TabelMatahari.bujurEkliptik(tau, nilaiT)[2]
+	val sunGeometricLongitudeDegreesDMS = toDegreeFullRound2(sunGeometricLongitudeDegrees)
+    
+	/**
+	 * sun geometric longitude lamdaM `
+	 */
+	val sunGeometricLonLamdaM = TabelMatahari.bujurEkliptik(tau, nilaiT)[3]
+	val sunGeometricLonLamdaMDMS = toDegreeFullRound2(sunGeometricLonLamdaM)
+    
+	/**
+	 * sun geometric longitude lamdaM ` radians
+	 */
+	val sunGeometricLonLamdaMRadians = TabelMatahari.bujurEkliptik(tau, nilaiT)[6]
+	val sunGeometricLonLamdaMRadiansDMS = toDegreeFullRound2(sunGeometricLonLamdaMRadians)
+    
+	/**
+	 * sun true geocentric equinox J2000 Degrees
+	 */
+	val sunTrueGeocentricJ2000Degrees = TabelMatahari.bujurEkliptik(tau, nilaiT)[5]
+    val sunTrueGeocentricJ2000DegreesDMS = toDegreeFullRound2(sunTrueGeocentricJ2000Degrees)
+    
+    /**
+	 * earth heliocentric latitude radians
+	 */
+	val earthHeliocentricLatitudeRadians = TabelMatahari.lintangEkliptikB(tau, sunGeometricLonLamdaMRadians)[0]
+    val earthHeliocentricLatitudeRadiansDMS = toDegreeFullRound2(earthHeliocentricLatitudeRadians)
+	
+	/**
+	 * koreksi delta theta untuk equinox dynamic FK5 system
+	 */
+	val deltaThetaDynamicFK5 = (-0.09033 + 0.03916 * (cos(sunGeometricLonLamdaM) + sin(sunGeometricLonLamdaM)) * tan(earthHeliocentricLatitudeRadians))
+	
+	/**
+	 * sun true geocentric FK5 Degrees
+	 */
+	val sunTrueGeocentricLonFK5Degrees = sunGeometricLongitudeDegrees + deltaThetaDynamicFK5
+    val sunTrueGeocentricLonFK5DegreesDMS = toDegreeFullRound2(sunTrueGeocentricLonFK5Degrees)
+	
+	
+	
+	/**
+	 * delta beta
+	 */
+	val deltaBeta = TabelMatahari.lintangEkliptikB(tau, sunGeometricLonLamdaMRadians)[1]
+	
+	/**
+	 * sun true geocentric latitude radians
+	 */
+	val sunTrueGeocentricLatitudeDegrees = TabelMatahari.lintangEkliptikB(tau, sunGeometricLonLamdaMRadians)[2]
+	val sunTrueGeocentricLatitudeDegreesDMS = toDegreeFullRound2(sunTrueGeocentricLatitudeDegrees)
+	/**
+	 * sun true geocentric latitude degrees
+	 */
+	val sunTrueGeocentricLatitudeRadians = (sunTrueGeocentricLatitudeDegrees * 180) / PI
+	val sunTrueGeocentricLatitudeRadiansDMS = toDegreeFullRound2(sunTrueGeocentricLatitudeRadians)
+	
+	/**
+	 * b_detikBusur
+	 */
+	val b_detikBusur = TabelMatahari.lintangEkliptikB(tau, sunGeometricLonLamdaMRadians)[3]
+    
+    /**
+    * vector radius jarak bumi matahari
+    */
+    val r = TabelMatahari.jarakBumiMat(tau)
     
     val test = Nutasi.deltaPsiDanEpsilon(nilaiT)[1]
 }
--- lib/src/test/kotlin/com/andihasan7/lib/ephemeris/jeanmeeus/EphemerisMeeusTest.kt
+++ lib/src/test/kotlin/com/andihasan7/lib/ephemeris/jeanmeeus/EphemerisMeeusTest.kt
@@ -9,13 +9,13 @@ class EphemerisMeeusTest {
     fun testEphemerisMeeus() {
         
         val jm = EphemerisMeeus(
-            10, // 20
+            20, // 20
             4, 
-            1987, // 2023
-            -7.029055555556,
-            106.557722222222,
-            0.0,
-            0.0, // 17.8575
+            2023, // 2023
+            -6.166666666667, //-7.029055555556,
+            106.85, //106.557722222222,
+            7.0,
+            17.8575,//17.828611111111, // 17.8575
             true
         )
         
@@ -31,6 +31,17 @@ class EphemerisMeeusTest {
         val meanObliquityOfEcliptic = jm.meanObliquityOfEcliptic
         val deltaEps = jm.deltaEps
         val trueObliquityOfEcliptic = jm.trueObliquityOfEcliptic
+		
+		val earthHeliocentricLongitudeDegrees = jm.earthHeliocentricLongitudeDegrees
+		val sunGeometricLongitudeDegrees = jm.sunGeometricLongitudeDegrees
+		val sunGeometricLonLamdaMRadians = jm.sunGeometricLonLamdaMRadians
+		val sunTrueGeocentricLonFK5Degrees = jm.sunTrueGeocentricLonFK5Degrees
+		val deltaBeta = jm.deltaBeta
+        val earthHeliocentricLatitudeRadians = jm.earthHeliocentricLatitudeRadians
+		val sunTrueGeocentricLatitudeRadians = jm.sunTrueGeocentricLatitudeRadians
+		val sunTrueGeocentricLatitudeDegrees = jm.sunTrueGeocentricLatitudeDegrees
+		val b_detikBusur = jm.b_detikBusur
+        val r = jm.r
         
         val test = jm.test
     
@@ -48,5 +59,17 @@ class EphemerisMeeusTest {
         println("mean obliquity        = $meanObliquityOfEcliptic, ${jm.meanObliquityOfEclipticDMS}")
         println("deltaEps              = $deltaEps, ${jm.deltaEpsDMS}")
         println("true obliquity        = $trueObliquityOfEcliptic, ${jm.trueObliquityOfEclipticDMS}")
+		println("")
+		println("earthHelioLonDeg      = $earthHeliocentricLongitudeDegrees, ${jm.earthHeliocentricLongitudeDegreesDMS}")
+        println("earthHelioLatRad      = $earthHeliocentricLatitudeRadians, ${jm.earthHeliocentricLatitudeRadiansDMS}")
+        println("sunGeoLonDeg          = $sunGeometricLongitudeDegrees, ${jm.sunGeometricLongitudeDegreesDMS}")
+        println("sunGeoLonLamdaNR      = $sunGeometricLonLamdaMRadians, ${jm.sunGeometricLonLamdaMRadiansDMS}")
+		println("sunTrueGeoFK5Deg      = $sunTrueGeocentricLonFK5Degrees, ${jm.sunTrueGeocentricLonFK5DegreesDMS}")
+        println("deltaBeta             = $deltaBeta, ${jm.deltaBeta}")
+        println("sunTrueGeoLatRad      = $sunTrueGeocentricLatitudeRadians, ${jm.sunTrueGeocentricLatitudeRadiansDMS}")
+		println("sunTrueGeoLatDeg      = $sunTrueGeocentricLatitudeDegrees, ${jm.sunTrueGeocentricLatitudeDegreesDMS}")
+        println("b_detikBusur          = $b_detikBusur, ${jm.b_detikBusur}")
+        println("vector radius         = $r, ${jm.r}")
+		
     }
 }
